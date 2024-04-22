3211e19acd7aed54b260cb1180eb48cb150e6bb3
Otxobot <otxoboy64@gmail.com>
2024-04-22 17:42:45 +0200

    tocando un mini servidor en c++

--- main.cpp
+++ main.cpp
@@ -60,16 +60,15 @@ int main() {
     char buffer[1024] = {0};
     recv(client_socket, buffer, sizeof(buffer), 0);
     std::cout << "Message from client: " << buffer << std::endl;
-    // Respond to client
-    // const char *message = "Hello, client! This is the server.";
-    // if (send(client_socket, message, strlen(message), 0) == -1) 
-    // {
-    //     std::cerr << "Send failed: " << strerror(errno) << std::endl;
-    // }
+    const char *message = "Hello, client! This is the server.";
+    if (send(client_socket, message, strlen(message), 0) == -1)
+    {
+        std::cerr << "Send failed: " << strerror(errno) << std::endl;
+    }
 
     // Close sockets
-    // close(client_socket);
-    // close(server_fd);
+    close(client_socket);
+    close(server_fd);
 
     return 0;
 }
