9807b8da1b472021d02a04201f3a6deccde0f80d
Otxobot <otxoboy64@gmail.com>
2024-04-29 12:16:19 +0200

    empezando servidor para cuando reciba el parseo

--- main.cpp
+++ main.cpp
@@ -1,66 +1,85 @@
-//#include "Webserv.hpp"
-#include <sys/socket.h> // For socket functions
-#include <netinet/in.h> // For sockaddr_in
-#include <cstdlib> // For exit() and EXIT_FAILURE
-#include <iostream> // For cout
-#include <unistd.h> // For read
-#include <cerrno>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:39:03 by abasante          #+#    #+#             */
+/*   Updated: 2024/04/29 11:57:20 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-  // Create a socket (IPv4, TCP)
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    std::cout << "Failed to create socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
+#include "Webserv.hpp"
+// #include <sys/socket.h> // For socket functions
+// #include <netinet/in.h> // For sockaddr_in
+// #include <cstdlib> // For exit() and EXIT_FAILURE
+// #include <iostream> // For cout
+// #include <unistd.h> // For read
+// #include <cerrno>
 
-  int optval = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
-    std::cout << "Failed to set socket option SO_REUSEADDR. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
+// int main() 
+// {
+//   // Create a socket (IPv4, TCP)
+//   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//   if (sockfd == -1) {
+//     std::cout << "Failed to create socket. errno: " << errno << std::endl;
+//     exit(EXIT_FAILURE);
+//   }
 
-  // Listen to port 8080 on any address
-  sockaddr_in sockaddr;
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = INADDR_ANY;
-  sockaddr.sin_port = htons(8080); // htons is necessary to convert a number to
-                                   // network byte order
+//   int optval = 1;
+//   if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
+//     std::cout << "Failed to set socket option SO_REUSEADDR. errno: " << errno << std::endl;
+//     exit(EXIT_FAILURE);
+//   }
 
-  if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) 
-  {
-    std::cout << "Failed to bind to port 8080. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
+//   // Listen to port 8080 on any address
+//   sockaddr_in sockaddr;
+//   sockaddr.sin_family = AF_INET;
+//   sockaddr.sin_addr.s_addr = INADDR_ANY;
+//   sockaddr.sin_port = htons(8080); // htons is necessary to convert a number to
+//                                    // network byte order
 
-  // Start listening. Hold at most 10 connections in the queue
-  if (listen(sockfd, 10) < 0) 
-  {
-    std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
+//   if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) 
+//   {
+//     std::cout << "Failed to bind to port 8080. errno: " << errno << std::endl;
+//     exit(EXIT_FAILURE);
+//   }
 
-  // Grab a connection from the queue
-  int addrlen = sizeof(sockaddr);
-  int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
-  if (connection < 0) 
-  {
-    std::cout << "Failed to grab connection. errno: " << errno << std::endl;
-    exit(EXIT_FAILURE);
-  }
+//   // Start listening. Hold at most 10 connections in the queue
+//   if (listen(sockfd, 10) < 0) 
+//   {
+//     std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+//     exit(EXIT_FAILURE);
+//   }
 
-  // Send a message to the connection
-  std::string response = "Hello good morning today, from gorliz";
+//   // Grab a connection from the queue
+//   int addrlen = sizeof(sockaddr);
+//   int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
+//   if (connection < 0) 
+//   {
+//     std::cout << "Failed to grab connection. errno: " << errno << std::endl;
+//     exit(EXIT_FAILURE);
+//   }
 
-  send(connection, response.c_str(), response.size(), 0);
+//   // Send a message to the connection
+//   std::string response = "Hello good morning today, from gorliz";
 
-  // Read from the connection
-  char buffer[1000];
-  read(connection, buffer, 1000);
-  std::cout << "The message was: \n" << buffer;
+//   send(connection, response.c_str(), response.size(), 0);
+
+//   // Read from the connection
+//   char buffer[1000];
+//   read(connection, buffer, 1000);
+//   std::cout << "The message was: \n" << buffer;
 
 
-  // Close the connections
-  close(connection);
-  close(sockfd);
+//   // Close the connections
+//   close(connection);
+//   close(sockfd);
+// }
+
+int main()
+{
+  //Este main es para probar montar el servidor que recibe el parseo de el archivo de configuración ya hecho.
+  
 }
--- server/Server.hpp
+++ server/Server.hpp
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:39:08 by abasante          #+#    #+#             */
+/*   Updated: 2024/04/29 12:16:02 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
 #include "Webserv.hpp"
+#include "../config/Config.hpp"
+
+class Config;
 
 class Server
 {
