37caef603840c0b6818d8dff91cf1b7c98946bb3
Otxobot <otxoboy64@gmail.com>
2024-04-30 13:31:00 +0200

    empezando config bien

---
+++ config/Location.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Location.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 13:26:21 by abasante          #+#    #+#             */
+/*   Updated: 2024/04/30 13:27:50 by abasante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
+
+#include "Webserv.hpp"
+#include "Config.hpp"
+
+class Location
+{
+	private:
+		bool			_allowGET;
+		bool			_allowPOST;
+		bool			_allowDELETE;
+	public:
+
+}
+
+#endif
