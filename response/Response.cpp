2b97a237d91ab341bbf05a8210bf56d54e92029c
Aiert Basante <otxoboy64@gmail.com>
2024-05-23 15:55:03 +0200

    response

--- response/Response.cpp
+++ response/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:00:31 by abasante          #+#    #+#             */
-/*   Updated: 2024/05/23 14:01:38 by abasante         ###   ########.fr       */
+/*   Updated: 2024/05/23 15:54:20 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,28 +72,6 @@ std::string Response::getStatusCodeTranslate()
     return status;
 }
 
-// void Response::makeBody()
-// {
-//     std::string path_to_requested = this->_request.getTarget();
-//     this->_isLocation = false;
-//     this->_isCGI = false;
-
-//     if (path_to_requested.empty())
-//         path_to_requested.append("/");
-//     for (size_t i = 0; i < this->_servers.size(); i++)
-//     {
-//         if (this->_servers[i].getPort() == this->_request.getPort())
-//         {
-//             this->_server = this->_servers[i];
-//             break;
-//         }
-//     }
-//     std::map<std::string, Location> locations = this->_server._locations;
-    
-//     std::vector<Location> all_locations;
-    
-// }
-
 // void    Response::get_body(std::string file_name)
 // {
 //     std::ifstream file(file_name);
@@ -132,8 +110,12 @@ Config Response::calibrate_host_location(std::vector<Config> _servers, Request _
 void Response::enter_location(Config server, std::string uri)
 {
     Location our_location;
+    if (uri.empty())
+        uri.append("/");
+    
+    std::cout << server._port << std::endl;
     our_location = server._locations[uri];
-    std::cout << our_location._file << std::endl;
+    std::cout <<"aasdfasdf->"<< our_location._file << std::endl;
 }
 
 void Response::responseCreation(std::vector<Config> &servers, Request &request)
@@ -157,7 +139,7 @@ void Response::responseCreation(std::vector<Config> &servers, Request &request)
         std::string uri = this->_request.getTarget();
         std::cout << uri << std::endl;
         this->enter_location(this->_server, uri);
-        
+        //this->check_for_statusCode();
         // int number = this->_statusCode;
         // std::ostringstream oss;
         // oss << number;
